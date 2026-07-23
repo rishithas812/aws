595104f5a2d4e052033247e705806a1b7cca6c21
S Rishitha <rishithavitphotos1@gmail.com>
2026-07-23 12:29:43 +0530

    Create subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,29 @@
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+
+  vpc_id = aws_vpc.employee_vpc.id
+
+  cidr_block = "10.0.1.0/24"
+
+  availability_zone = "ap-south-1a"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public-Subnet"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+
+  vpc_id = aws_vpc.employee_vpc.id
+
+  cidr_block = "10.0.2.0/24"
+
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "Private-Subnet"
+  }
+}
