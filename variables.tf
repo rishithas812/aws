1a52abe9f78ef9037aa07a3a39ff539626cf051f
S Rishitha <rishithavitphotos1@gmail.com>
2026-07-23 12:29:01 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "ami" {
+  default = "ami-0f918f7e67a3323f0"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  default = "MyTest-Key"
+}
